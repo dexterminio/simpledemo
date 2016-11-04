5dfb2227e6e828e8f1797c9e959879015e41d20b
dexterminio <dexterminio@hotmail.com>
2016-11-03 23:10:30 -0700

    small changes

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ app.use('/', express.static(__dirname + '/assets'));
 app.engine('html', require('atpl').__express);
 
 app.set('view engine','html');
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 01406ee12c381582d22bce654737ecdab6112d2d
 // listen
 app.listen(PORT, function(){
     console.log('Ready: http://localhost:'+PORT+'/');
