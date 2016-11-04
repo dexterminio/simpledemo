01406ee12c381582d22bce654737ecdab6112d2d
gfloresuth <dexterminio@gmail.com>
2016-11-03 22:58:40 -0700

    small change

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.engine('html', require('atpl').__express);
 app.set('view engine','html');
 
 
+// listen
 app.listen(PORT, function(){
     console.log('Ready: http://localhost:'+PORT+'/');
 });
