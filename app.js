f85757d700a1ab318411de91e9bfc8416013d062
gfloresuth <dexterminio@gmail.com>
2016-11-04 15:05:25 -0700

    styles styles

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const PORT = 3000;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', express.static(__dirname + '/assets'));
+app.use('/', express.static(__dirname + '/public'));
 app.engine('html', require('atpl').__express);
 
 app.set('view engine','html');
+
+
 
 
 // listen in the port
