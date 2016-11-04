8dea92d18c4275fc29564aee5465fb411a218a8a
gfloresuth <dexterminio@gmail.com>
2016-11-03 20:20:54 -0700

    app.js changes

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const PORT = 3000;
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', express.static(__dirname + '/assets'));
+app.engine('html', require('atpl').__express);
+
+app.set('view engine','html');
+
+
+app.listen(PORT, function(){
+    console.log('Ready: http://localhost:'+PORT+'/');
+});
